784702103fc421e34a7219590036ffedb33380f7
jaaw1 <jaani.nurisalo@edu.turkuamk.fi>
2022-12-02 00:52:34 +0200

    2021_02 part 1 done

---
+++ 2021_02.py
@@ -0,0 +1,27 @@
+
+file = open('2021_02_input.txt', 'r')
+file2 = open('2021_02_test.txt', 'r')
+Lines = file.readlines()
+
+count = 0
+vertical = 0
+horizontal = 0
+direction = 'UP'
+
+# Strips the newline character
+for line in Lines:
+    for word in line.split():
+        if word.isnumeric():
+            if direction.upper() == 'UP':
+                vertical -= int(word)
+            elif direction.upper() == 'DOWN':
+                vertical += int(word)
+            else:
+                horizontal += int(word)
+        else:
+            direction = word
+
+print(horizontal, vertical, horizontal*vertical)
+
+
+
