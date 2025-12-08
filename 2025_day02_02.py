de371eb16079a19e67a4777dfa813c0246e1457c
jaaw1 <jaani.nurisalo@edu.turkuamk.fi>
2025-12-09 00:24:14 +0200

    2025 day 2 part 2 finished!

--- 2025_day02_02.py
+++ 2025_day02_02.py
@@ -11,6 +11,7 @@ def split_and_check(id):
     else:
         return 0
 
+
 def multiples_check(id, rounds):
     x = len(str(id))
     if x < rounds:
@@ -20,14 +21,14 @@ def multiples_check(id, rounds):
     else:
         round_len = x // rounds
         parts = [str(id)[i:i+round_len] for i in range(0, x, round_len)]
-        test = 0
+
         for y in range(1, len(parts)):
+
             if parts[y] != parts[0]:
-                test += 1
+
+                return 0
 
         return 1
-
-
 
 
 def loop(separated):
@@ -46,32 +47,12 @@ def loop(separated):
             multi_counter += multiples_check(x, 7)
             if multi_counter > 0:
                 counter += x
-                print(x, id)
 
     print(counter)
 
-testi = "banana"
-
-#ee = ','.join(testi)
-#print(ee)
-#print(testi[:len(str(testi)) // 3])
-#print(testi[:len(str(testi)) // 3:])
-#print(testi[len(str(testi)) // 3:])
-#input("eee")
-
-ugh = 0
-lissu = ['e', 'e', 'm']
-
-for u in range(1, len(lissu)):
-    if lissu[u] != lissu[0]:
-        print(lissu[u])
-        ugh += 1
-
-if ugh > 0:
-    input("ADD TO COUNTER")
 
 separated = TestLines[0].split(",")
 loop(separated)
-"""
+
 separated = Lines[0].split(",")
-loop(separated)"""
+loop(separated)
