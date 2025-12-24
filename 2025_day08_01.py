5e47c39cb9646a2181b37df80f1d755685937b03
jaaw1 <jaani.nurisalo@edu.turkuamk.fi>
2025-12-24 02:04:43 +0200

    2025 day08 01 edistetty

--- 2025_day08_01.py
+++ 2025_day08_01.py
@@ -16,3 +16,18 @@ def create_3d_vectors(data):
     return np.array(vectors)
 
 print(create_3d_vectors(TestLines))
+
+
+
+p1 = np.array((1, 2, 3))
+p2 = np.array((1, 1, 1))
+
+d = np.linalg.norm(p1 - p2)
+print(d)
+
+
+class Vector_groups:
+    def __init__(self, data):
+        self.group = np.array([data])
+
+
