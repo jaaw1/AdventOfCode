0c266b01ac86570fb080299cf31cacfc3e218f2a
jaaw1 <jaani.nurisalo@edu.turkuamk.fi>
2025-12-29 01:00:24 +0200

    Merging groups works

--- 2025_day08_01.py
+++ 2025_day08_01.py
@@ -1,11 +1,17 @@
 import numpy as np
+print(np.__version__)
 import math
-from streamlit.runtime.stats import group_stats
+#from streamlit.runtime.stats import group_stats
 
 testdata = open('2025_day08_test.txt', 'r')
 data = open('2025_day08_text.txt', 'r')
 TestLines = testdata.readlines()
 Lines = data.readlines()
+
+ss = np.array(([[1, 2, 3], [7, 8, 9]]))
+print(ss)
+sss = np.append(ss, np.array(([[4, 5, 6]])), axis=0)
+print(sss)
 
 
 def create_3d_vectors(data):
@@ -17,16 +23,44 @@ def create_3d_vectors(data):
 
     return np.array(vectors)
 
-points = create_3d_vectors(Lines)
 
-print(points)
+def merge_groups(groups, ind1, ind2):
+
+    a, b = groups[ind1], groups[ind2]
+    print(len(a.data.shape))
+    if len(a.data.shape) > 1:
+        a.data = np.append(a.data, [b.data], axis=0)
+    else:
+        a.data = np.append([a.data], [b.data], axis=0)
+    print(len(a.data.shape))
+
+    groups.pop(ind2)
+    return a.data
+
+
+
+points = create_3d_vectors(TestLines)
+
+rem = np.array([points[0]])
+input(rem)
+drem = np.append(rem, np.array([points[1]]), axis=0)
+input(drem)
+mer = np.array([points[2]])
+dmer = np.append(mer, np.array([points[3]]), axis=0)
+print(dmer)
+merde = np.append(drem, dmer, axis=0)
+input(merde)
+
+
+
+#print(points)
 
 
 p1 = np.array((1, 2, 3))
 p2 = np.array((1, 1, 1))
 
 d = np.linalg.norm(p1 - p2)
-print(d)
+#print(d)
 """
 import numpy as np
 from sklearn.cluster import DBSCAN
@@ -61,29 +95,102 @@ class Groups:
             #print(distances)
         return min(distances)
 
+
+
+
+
     def is_vector_in_this_group(self, vector):
+        print(self.data, vector, self.data == vector)
         return any(np.array_equal(vector, x) for x in self.data)
 
 
     def __str__(self):
-        return f'Size: {self.size}\\nLast: {self.data[-1]}'
+        return f'Size: {self.size}  Last: {self.data[-1]}'
+
+
+for i in range(len(points)):
+    group = Groups(points[i], i)
+    all_groups.append(group)
+
+
+
+print(merge_groups(all_groups, 0, 1))
+input("115 wait")
+
+#CONTINUE HERE
+
+# 1. loop points
+    #for every point, find its closest neighbor
+# 2. Find group of closest neighbor
+# 3 Find group of the point in the loop in turn
+# 4. Merge these two groups
+
+#
+"""
+for x in range(len(points)):
+
+    nearest_group = 10000000
+    nearest_id = 1001
+    native = 1001
+
+    # get native group
+    for i in range(len(all_groups)):
+        isit = all_groups[i].is_vector_in_this_group(points[x])
+        print(isit, points[x], all_groups[i].data)
+        if isit:
+            native = i
+            print("Native: ", native)
+            break
+
+    for y in range(len(all_groups)):
+        if y == i:
+            pass
+        else:
+            nearest = all_groups[y].shortest_distance_to_vector_in_group(points[x])
+        if nearest < nearest_group:
+            nearest_group == nearest
+            nearest_id = y
+
+    merge_groups(all_groups, native, nearest_id)
+"""
+
+
 
 
 d = Groups(list([points[0], points[1]]), 1)
-print(d)
-
+print("rivi 129", d)
+print("Name: ", type(d).__name__)
 r = d.shortest_distance_to_vector_in_group(points[2])
-print(r)
+print("rivi 132", r)
+
+dd = Groups(list([points[0]]), 1)
+print("rivi 135", dd.data[0])
+print("rivi 136", d.is_vector_in_this_group(dd.data[0]))
+
+ff = Groups(list([points[2], points[3]]), 1)
+gg = Groups(list([points[4], points[5]]), 1)
+
+all_groups.append(ff)
+all_groups.append(gg)
+
+#tt = ff.merge_groups(gg)
+tt = merge_groups(all_groups, 0, 1)
+
+print(tt)
 
 h = d.is_vector_in_this_group(points[0])
-print(h)
+print("rivi 146", h)
 
 hh = d.is_vector_in_this_group(points[8])
-print(hh)
+print( "rivi 149", hh)
 
+input("wait")
 group_all = Groups(list(points[:]), 0)
 print(group_all)
 for i in range(len(points)):
     vector = points[i]
     print(vector)
-    input("wait")
+    reve = group_all.is_vector_in_this_group(vector)
+    print(reve)
+
+    #input("wait")
